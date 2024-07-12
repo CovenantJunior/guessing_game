e316e95ce848fe054cc88cc9e2b6211392591157
CovenantJunior <covenanttope@gmail.com>
2024-07-12 11:40:18 +0100

    Make used of placeholders

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
@@ -16,6 +16,6 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+    println!("You guessed: {}", guess);
 
 }
